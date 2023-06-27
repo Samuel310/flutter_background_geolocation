598f843f53ed2b778c23893c8dfae40610709e35
Samuel310 <samuelbenjamin310@gmail.com>
2023-06-27 14:36:42 +0530

    removed android source

--- android/src/main/java/com/transistorsoft/flutter/backgroundgeolocation/FLTBackgroundGeolocationPlugin.java
+++ android/src/main/java/com/transistorsoft/flutter/backgroundgeolocation/FLTBackgroundGeolocationPlugin.java
@@ -1,63 +1,46 @@
 package com.transistorsoft.flutter.backgroundgeolocation;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.PluginRegistry;
+
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.Result;
 
 /**
  * FlutterBackgroundGeolocationPlugin
  */
-public class FLTBackgroundGeolocationPlugin implements FlutterPlugin, ActivityAware {
+public class FLTBackgroundGeolocationPlugin implements FlutterPlugin, MethodCallHandler  {
 
-    /** Plugin registration. */
-    public static void registerWith(PluginRegistry.Registrar registrar) {
-        BackgroundGeolocationModule module = BackgroundGeolocationModule.getInstance();
-        module.onAttachedToEngine(registrar.context(), registrar.messenger());
-        if (registrar.activity() != null) {
-            module.setActivity(registrar.activity());
-        }
-    }
+  private MethodChannel channel;
 
-    // @deprecated Called by Application#onCreate
-    public static void setPluginRegistrant(PluginRegistry.PluginRegistrantCallback callback) {
-        HeadlessTask.setPluginRegistrant(callback);
-    }
+  private static final String TAG = "BG_PLUGIN";
 
-    public FLTBackgroundGeolocationPlugin() { }
+  public FLTBackgroundGeolocationPlugin() {
+    Log.d(TAG, "FLTBackgroundGeolocationPlugin: Constructor called");
+  }
 
-    @Override
-    public void onAttachedToEngine(FlutterPlugin.FlutterPluginBinding binding) {
-        BackgroundGeolocationModule.getInstance().onAttachedToEngine(binding.getApplicationContext(), binding.getBinaryMessenger());
-    }
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "com.transistorsoft/flutter_background_geolocation/methods");
+    channel.setMethodCallHandler(this);
+  }
 
-    @Override
-    public void onDetachedFromEngine(@NonNull FlutterPlugin.FlutterPluginBinding binding) {
-        BackgroundGeolocationModule.getInstance().onDetachedFromEngine();
-    }
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    
+  }
 
-    @Override
-    public void onAttachedToActivity(ActivityPluginBinding activityPluginBinding) {
-        BackgroundGeolocationModule.getInstance().setActivity(activityPluginBinding.getActivity());
-    }
-
-    @Override
-    public void onDetachedFromActivityForConfigChanges() {
-        // TODO: the Activity your plugin was attached to was
-        // destroyed to change configuration.
-        // This call will be followed by onReattachedToActivityForConfigChanges().
-    }
-
-    @Override
-    public void onReattachedToActivityForConfigChanges(ActivityPluginBinding activityPluginBinding) {
-        // TODO: your plugin is now attached to a new Activity
-        // after a configuration change.
-    }
-
-    @Override
-    public void onDetachedFromActivity() {
-        BackgroundGeolocationModule.getInstance().setActivity(null);
-    }
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
 }
